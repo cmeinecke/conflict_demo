3af631847815608ac8437ccb201062e4c416931f
Colin Meinecke <cmeinecke@gmail.com>
2022-02-10 15:09:07 -0600

    merge

--- helloworld.py
+++ helloworld.py
@@ -1,26 +1,9 @@
-<< << << < HEAD
-
-
 def hello_world(city, state):
     print(f'Hello World {city}, {state}!')
 
 
 def main():
     hello_world('NYC', 'New York')
-
-
-== == == =
-
-
-def hello_world(city):
-    print(f'Hello World {city}!')
-
-
-def main():
-    hello_world('NYC')
-
-
->>>>>> > 5fff235ce03f599387a8c9c5a3bd986640145e85
 
 
 main()
